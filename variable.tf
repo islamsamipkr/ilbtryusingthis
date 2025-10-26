8e0b3f384809a092298b0dab0edcc8d6a2a85a81
Sami Islam <islam.samipkr@gmail.com>
2025-10-26 12:21:43 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,61 @@
+# variables.tf
+
+variable "project_name" {
+  description = "The short name of the project/application"
+  type        = string
+  default     = "moba025ebill"
+}
+
+variable "region" {
+  description = "Default region for resources"
+  type        = string
+  default     = "US"
+}
+
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+  default     = "cnr-moba025ebill-dev-d28b"
+}
+
+# Global HTTPS Load Balancer configuration
+variable "global_ip_address_name" {
+  description = "Name of the global IP address for the load balancer"
+  type        = string
+  default     = "ebill"
+}
+
+variable "loadbalancername" {
+  description = "Name of the HTTPS load balancer"
+  type        = string
+  default     = "ebill-httpsserverless-loadbalancer"
+}
+
+variable "app" {
+  description = "Application name"
+  type        = string
+  default     = "ebill"
+}
+
+variable "env" {
+  description = "Deployment environment (dev, qa, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "hosts" {
+  description = "List of hostnames to associate with the load balancer"
+  type        = list(string)
+  default     = ["srv.ebill-fb-dev.web.cn.ca"]
+}
+
+variable "ssl" {
+  description = "Whether SSL is enabled for the load balancer"
+  type        = bool
+  default     = true
+}
+
+variable "dns_suffix" {
+  description = "DNS suffix used for the environment"
+  type        = string
+  default
