23adbf581e020bd6294e6087ba529ac777a46327
Sami Islam <islam.samipkr@gmail.com>
2025-10-26 12:36:41 -0400

    Create reserveipaddress.tf

---
+++ reserveipaddress.tf
@@ -0,0 +1,6 @@
+resource "google_compute_global_address" "cloudfunctionip" {
+  name         = "global-${var.global_ip_address_name}-cloudfunction-ip"
+  project      = var.project_id
+  address_type = "INTERNAL"
+  ip_version   = "IPV4"
+}
