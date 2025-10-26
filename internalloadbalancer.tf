8f2d10a83be161057dbba876b496a0ab440245f0
Sami Islam <islam.samipkr@gmail.com>
2025-10-26 12:04:39 -0400

    Create internalloadbalancer.tf

---
+++ internalloadbalancer.tf
@@ -0,0 +1,18 @@
+locals {
+  config  = yamldecode(file("./configs/${var.env}/ilb.yaml"))
+}
+
+module "ilb" {
+  source = "./configs/"
+
+  google_project           = var.project_id
+  google_labels            = var.google_labels
+  project_name             = var.project_name
+  domain                   = var.hosts[0]
+  glb_ip_address           = google_compute_global_address.cloudfunctionip.address
+  url_map_name             = var.loadbalancername
+  neg_regions              = var.neg_region_list
+  cloudarmorlink           = var.cloudarmorlink
+  functions_map            = local.config
+  default_backend_function = var.default_backend_function
+}
