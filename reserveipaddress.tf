5490600eb4850e627b4c24ee4fe17e0084ef2eaa
Sami Islam <islam.samipkr@gmail.com>
2025-10-27 08:57:20 -0400

    Update reserveipaddress.tf

--- reserveipaddress.tf
+++ reserveipaddress.tf
@@ -1,8 +1,8 @@
-resource "google_compute_global_address" "cloudfunctionip" {
+resource "google_compute_address" "cloudfunctionip" {
   name         = "global-${var.global_ip_address_name}-cloudfunction-ip"
   project      = var.project_id
   address_type = "INTERNAL"
   ip_version   = "IPV4"
 purpose      = "GCE_ENDPOINT"
-
+ region  =var.region
 }
